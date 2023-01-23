c88fc799d7f435a2134ad6d8aa9d71885442dab1
Ajit Kumar Mishra <117336878+Ajit-kumar-mishra@users.noreply.github.com>
2023-01-23 08:55:05 +0530

    convert lowercase alphabet into uppercase alphabet

---
+++ uppercase.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+char str[100], upper_str[100];
+int i=0;
+printf("\n Enter the string :");
+gets(str);
+while(str[i] != '\0')
+{
+if(str[i]>='a' && str[i]<='z')
+upper_str[i] = str[i] - 32;
+ else
+upper_str[i] = str[i];
+ i++;
+}
+upper_str[i] = '\0';
+printf("\n The string converted into upper case is : ");
+puts(upper_str);
+return 0;
+}
